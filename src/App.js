e72aa3c2e0aeb141e0549bc893fc9b41bfa96762
Alonsop <alonsop@uakami.com>
2018-07-14 14:03:36 -0400

    Creating the Simple TodoApp with the methods to change, delete and submit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import List from './List';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+      items:[]
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term:'',
+      items: [...this.state.items, this.state.term]
+    });
+  };
+
+  onDelete = (index) => {
+    const items = [...this.state.items];
+    items.splice(index, 1);
+    this.setState({ items });
+  };
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App offset-3 col-md-6">
+        <form onSubmit={this.onSubmit} className="form-group">
+          <h1>Simple TodoApp</h1>
+          <input className="form-control mt-2 mb-3" onChange={this.onChange} />
+          <button className="btn btn-primary">Submit</button>
+        </form>
+        <List
+          items={this.state.items}
+          delete={this.onDelete} />
       </div>
     );
   }
